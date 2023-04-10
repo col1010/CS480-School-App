90a118616d0df54a1a64a59e1597fecfd1462798
Coleby Kauffman <kauffman.coleby@gmail.com>
2023-04-09 23:49:39 -0700

    Format correct config file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'LewistonSchools',
+  appId: 'com.LewistonSchools.EventApp',
+  appName: 'Lewiston School District Events',
   webDir: 'www',
   bundledWebRuntime: false
 };
