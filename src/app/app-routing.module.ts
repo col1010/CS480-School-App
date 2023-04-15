d9c971f63b696b98ab8a05fe4088d908893f8160
Coleby Kauffman <kauffman.coleby@gmail.com>
2023-04-14 18:44:59 -0700

    Begin adding tabs to the app. Very broken

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,36 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'calendar/:calendarId/event/:eventId',
-    loadChildren: () => import('./view-event/view-event.module').then( m => m.ViewEventPageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-];
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: TabsComponent,
+        children: [
+          {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'home',
+          },
+          {
+            path: 'home',
+            loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+          },
+          {
+            path: 'radio',
+            loadChildren: () => import('./feed/feed.module').then((m) => m.FeedPageModule),
+          },
+          {
+            path: 'calendar/:calendarId/event/:eventId',
+            loadChildren: () => import('./view-event/view-event.module').then( m => m.ViewEventPageModule)
+          }
+        ],
+      },
+    ]),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
+
 export class AppRoutingModule { }
