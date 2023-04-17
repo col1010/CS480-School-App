d9e39bb4a2fd5ee8a6f99612f70a73082c6e1aec
Coleby Kauffman <kauffman.coleby@gmail.com>
2023-04-17 02:22:54 -0700

    Move alert to tav nav page, add subscription to detect selected calendars, move checkbox change detection to menu component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,17 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { AlertController } from '@ionic/angular';
 import { Platform } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { CalendarService } from '../app.module';
 
-import { Calendar, CalendarService, Event } from '../services/data.service';
+import { Calendar, Event } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
 
   eventList: Event[] = [];
   envCalList: any[] = [];
@@ -19,16 +20,18 @@ export class HomePage implements OnInit {
 
   endIndex: number;
 
+  subscription: Subscription = new Subscription();
+
   moreEventsButtonDisabled: boolean;
   moreEventsButtonShown: boolean;
 
   dateOptions: any;
 
-  constructor(public calService: CalendarService, private alertController: AlertController, private platform: Platform) {
+  constructor(public calService: CalendarService, private platform: Platform) {
     this.endIndex = 15;
     this.moreEventsButtonDisabled = false;
     this.moreEventsButtonShown = false;
-
+    this.envCalList = environment.calendars;
     this.dateOptions = {
       weekday: 'long',
       month: 'long',
@@ -39,33 +42,26 @@ export class HomePage implements OnInit {
 
   async ngOnInit() {
     await this.platform.ready();
+    this.subscription = this.calService.selectedCalendarsChanged.subscribe(() => {
+      console.log("Changed (home component)!");
+      this.endIndex = 15;
+      this.moreEventsButtonDisabled = false;
+      this.refreshEvents();
+    });
     await this.initializeCalendars();
     this.refreshEvents();
   }
 
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
+  }
+
   initializeCalendars(): Promise<void> {
     return new Promise<void>(async (resolve) => {
-      this.envCalList = environment.calendars;
 
-      if (!localStorage.getItem("firstOpen")) { // first time opening the app
-        const defaultCalendarName = 'Independent School District #1';
-        const defaultCalendar = this.envCalList.find((cal) => cal.names.includes(defaultCalendarName));
-        if (defaultCalendar) {
-          this.selectedCalendars.push(defaultCalendar);
-          localStorage.setItem(defaultCalendarName, 'checked');
-        }
-        const alert = await this.alertController.create({
-          header: 'Welcome to the Independent School District Calendar App!',
-          message: 'You can select school calendars to view using the menu on the left. By default, only the district calendar has been selected.',
-          buttons: ['Got it!']
-        });
-        await alert.present();
-        localStorage.setItem("firstOpen", "false");
-      }
       for (let i = 0; i < this.envCalList.length; i++) {
         if (localStorage.getItem(this.envCalList[i].names[0]) === "checked") {
           // console.log(this.envCalList[i].names[0], " is checked!");
-          this.selectedCalendars.push(this.envCalList[i].names[0]);
           this.calService.addCalendar(new Calendar(this.envCalList[i].names[0], true));
         } else {
           this.calService.addCalendar(new Calendar(this.envCalList[i].names[0], false));
@@ -83,10 +79,11 @@ export class HomePage implements OnInit {
   }
 
   async refreshEvents() {
+    console.log("Refreshing events...");
     const combinedEventList = (await this.calService.getCalendarList()).reduce((acc, cal) => acc.concat(cal.eventLists), [] as Event[][]);
     this.eventList = await this.sortEvents(([] as Event[]).concat(...combinedEventList));
     this.dateList = Array.from(new Set(this.getEvents().map(event => CalendarService.formatDate(event.startDateObject))));
-    // console.log("combinedEventList: ", combinedEventList);
+     console.log("combinedEventList: ", combinedEventList);
     // console.log("eventList: ", this.eventList);
     // console.log("dateList:", this.dateList);
   }
@@ -118,20 +115,5 @@ export class HomePage implements OnInit {
   loadMoreEvents() {
     this.endIndex += 15;
     this.refreshEvents();
-  }
-
-  async onCheckboxChange(event: any, cal: any) {
-    const isChecked = event.detail.checked;
-    this.moreEventsButtonDisabled = false;
-    this.endIndex = 15;
-    if (isChecked) {
-      localStorage.setItem(cal.names[0], "checked");
-    } else {
-      localStorage.setItem(cal.names[0], "unchecked");
-    }
-    this.calService.changeCheckedStatus(cal.names[0], isChecked);
-    //console.log("Changing events...");
-    this.refreshEvents();
-    //console.log("Done");
   }
 }
