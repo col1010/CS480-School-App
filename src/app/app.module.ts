df4a03ee6850a7698dc0a39381f52bc22c100d77
Coleby Kauffman <kauffman.coleby@gmail.com>
2023-04-17 02:20:43 -0700

    Provide the CalendarService in app module for whole app to use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TabnavPageModule } from './tabnav/tabnav.module';
 
-
-
+import { CalendarService } from './services/data.service';
+export { CalendarService } from './services/data.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, TabnavPageModule],
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, CalendarService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
